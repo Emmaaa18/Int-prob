73a3744b517a4eeb3321e3fa5c8c7ee8631a8abe
Emmaaa18 <98335189+Emmaaa18@users.noreply.github.com>
2022-04-08 19:15:12 -0500

    Add files via upload

---
+++ Quiz 2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "utils.h"
+
+int main(){
+
+    user markovito;
+    markovito.name = "markovito";
+    markovito.type = "ServiceRobot"
+    markovito.height = 1,2;
+    markovito.weight = 2,5;
+    markovito.degreesOFFreedom = 3;
+
+    markovito . sayHelloToRobot = sayHelloToRobot;
+
+    user juan;
+    juan.Name = "juan";
+    juan.type = "ServiceRobot";
+    juan.height = 1,7;
+    juan.weight = 2,8;
+    juan.degreesOFFreedom = sayHelloRobot;
+
+    markovito.sayHelloToRobot(tiago);
+
+    return 0;
+
+
+}
